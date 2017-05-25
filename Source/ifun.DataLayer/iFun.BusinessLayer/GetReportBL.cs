bdff7f07775722d052061f88af782d2c95e883a0
kirubams <kirubanandhan.subramaniam@gmail.com>
2017-05-25 00:33:01 -0500

    added for testing

--- Source/ifun.DataLayer/iFun.BusinessLayer/GetReportBL.cs
+++ Source/ifun.DataLayer/iFun.BusinessLayer/GetReportBL.cs
@@ -10,7 +10,7 @@ namespace iFun.BusinessLayer
 {
     public class GetReportBL
     {
-        ///////
+      
         //public List<IncomeTransactionDTO> GetDailyIncomeReport(DateTime ReportDate)
         //{
         //    try
