e63becb900feecd80c18a73a851cb8ab34bbc9c9
kirubams <kirubanandhan.subramaniam@gmail.com>
2017-05-30 23:25:31 -0500

    UI designer

--- Source/ifun.DataLayer/iFunGameStation/Reports.Designer.cs
+++ Source/ifun.DataLayer/iFunGameStation/Reports.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reports));
             this.btnGetDailyIncome = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,7 +41,10 @@
             // 
             // btnGetDailyIncome
             // 
-            this.btnGetDailyIncome.Location = new System.Drawing.Point(76, 88);
+            this.btnGetDailyIncome.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGetDailyIncome.BackgroundImage")));
+            this.btnGetDailyIncome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetDailyIncome.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnGetDailyIncome.Location = new System.Drawing.Point(13, 17);
             this.btnGetDailyIncome.Name = "btnGetDailyIncome";
             this.btnGetDailyIncome.Size = new System.Drawing.Size(259, 79);
             this.btnGetDailyIncome.TabIndex = 0;
@@ -51,41 +55,53 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 54);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(297, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.Size = new System.Drawing.Size(250, 32);
             this.label1.TabIndex = 1;
             this.label1.Text = "Income For Today:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(366, 116);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(300, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 17);
+            this.label2.Size = new System.Drawing.Size(255, 32);
             this.label2.TabIndex = 2;
             this.label2.Text = "ExpenseForToday:";
             // 
             // lblDailyIncome
             // 
             this.lblDailyIncome.AutoSize = true;
-            this.lblDailyIncome.Location = new System.Drawing.Point(531, 54);
+            this.lblDailyIncome.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDailyIncome.Image = ((System.Drawing.Image)(resources.GetObject("lblDailyIncome.Image")));
+            this.lblDailyIncome.Location = new System.Drawing.Point(622, 8);
             this.lblDailyIncome.Name = "lblDailyIncome";
-            this.lblDailyIncome.Size = new System.Drawing.Size(16, 17);
+            this.lblDailyIncome.Size = new System.Drawing.Size(42, 46);
             this.lblDailyIncome.TabIndex = 3;
             this.lblDailyIncome.Text = "0";
             // 
             // lblDailyExpense
             // 
             this.lblDailyExpense.AutoSize = true;
-            this.lblDailyExpense.Location = new System.Drawing.Point(531, 116);
+            this.lblDailyExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDailyExpense.Image = ((System.Drawing.Image)(resources.GetObject("lblDailyExpense.Image")));
+            this.lblDailyExpense.Location = new System.Drawing.Point(622, 78);
             this.lblDailyExpense.Name = "lblDailyExpense";
-            this.lblDailyExpense.Size = new System.Drawing.Size(16, 17);
+            this.lblDailyExpense.Size = new System.Drawing.Size(42, 46);
             this.lblDailyExpense.TabIndex = 4;
             this.lblDailyExpense.Text = "0";
             // 
             // panel1
             // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.Controls.Add(this.btnGetDailyIncome);
             this.panel1.Controls.Add(this.btnDownloadDaily);
             this.panel1.Controls.Add(this.lblDailyIncome);
             this.panel1.Controls.Add(this.lblDailyExpense);
@@ -93,12 +109,15 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(25, 34);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(884, 210);
+            this.panel1.Size = new System.Drawing.Size(884, 266);
             this.panel1.TabIndex = 5;
             // 
             // btnDownloadDaily
             // 
-            this.btnDownloadDaily.Location = new System.Drawing.Point(602, 54);
+            this.btnDownloadDaily.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDownloadDaily.BackgroundImage")));
+            this.btnDownloadDaily.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownloadDaily.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnDownloadDaily.Location = new System.Drawing.Point(13, 128);
             this.btnDownloadDaily.Name = "btnDownloadDaily";
             this.btnDownloadDaily.Size = new System.Drawing.Size(259, 79);
             this.btnDownloadDaily.TabIndex = 6;
@@ -110,8 +129,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1008, 683);
-            this.Controls.Add(this.btnGetDailyIncome);
             this.Controls.Add(this.panel1);
             this.MaximizeBox = false;
             this.Name = "Reports";
--- Source/ifun.DataLayer/iFunGameStation/Reports.cs
+++ Source/ifun.DataLayer/iFunGameStation/Reports.cs
@@ -11,6 +11,7 @@ using iFun.BusinessLayer;
 using iFun.DTO;
 using System.IO;
 using iFun.Utilities;
+using System.Configuration;
 
 namespace iFunGameStation
 {
@@ -38,21 +39,25 @@ namespace iFunGameStation
             ExpenseTransactionBL expenTran = new ExpenseTransactionBL();
             var expenseTranList = expenTran.GetDailyExpenseTransaction(DateTime.Now);
 
-            using (var textWriter = File.CreateText(@"C:\Projects\iFun\Reports\DailyReport\IncomeTransaction.csv"))
+            string ReportDownloadPath = ConfigurationManager.AppSettings["ReportDailyDownload"];
+            string IncomeTranDownloadPath = ReportDownloadPath + "\\IncomeTransaction_" + System.DateTime.Now.ToLongDateString()+".csv";
+            using (var textWriter = File.CreateText(IncomeTranDownloadPath))
             {
                 foreach (var line in TOCSV.ToCsv(incomeTranlist))
                 {
                     textWriter.WriteLine(line);
+                    
                 }
             }
-
-            using (var textWriter = File.CreateText(@"C:\Projects\iFun\Reports\DailyReport\ExpenseTransaction.csv"))
+            string ExpenseTranDownloadPath = ReportDownloadPath + "\\ExpenseTransaction_" + System.DateTime.Now.ToLongDateString() + ".csv";
+            using (var textWriter = File.CreateText(ExpenseTranDownloadPath))
             {
                 foreach (var line in TOCSV.ToCsv(expenseTranList))
                 {
                     textWriter.WriteLine(line);
                 }
             }
+            MessageBox.Show("Reports Downloaded to" + ReportDownloadPath);
 
         }
 
